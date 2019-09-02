d77740a0c34d7fa6106170de692b29b1b2cc5243
Huy Do <huyddo40@gmail.com>
2019-09-02 21:34:27 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+# Your application should only accept the /items/<ITEM NAME> route. Everything else should 404
+# If a user requests /items/<Item Name> it should return the price of that item
+# IF a user requests an item that you don't have, then return a 400 and an error message
+
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      resp.write ""
+    else
+      resp.write "Item is not found"
+      resp.status = 400
+    end
+    resp.finish
+end
